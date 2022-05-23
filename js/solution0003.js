d05744f2dfe8e6b2245791e86e50baafb749bb3b
KevinmaSchmitz <kevinmaschmitz@gmail.com>
2022-05-23 09:29:19 -0500

    day 3 solution

---
+++ js/solution0003.js
@@ -0,0 +1,19 @@
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+const stringToNumber = function (str) {
+    return parseInt(str, 10)
+}
+
+
+// Tests
+
+describe("stringToNumber", function () {
+    it("should work for the examples", function () {
+        Test.assertEquals(stringToNumber("1234"), 1234)
+        Test.assertEquals(stringToNumber("605"), 605)
+        Test.assertEquals(stringToNumber("1405"), 1405)
+        Test.assertEquals(stringToNumber("-7"), -7)
+    });
+});
