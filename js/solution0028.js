344309179aa716f382afc4b40e2d8259d2d91e61
KevinmaSchmitz <kevinmaschmitz@gmail.com>
2022-09-11 09:37:01 -0500

    daily codwars

---
+++ js/solution0028.js
@@ -0,0 +1,18 @@
+// Given a non - empty array of integers, return the result of multiplying the values together in order. 
+
+function grow(x) {
+    return x.reduce((acc, c) => acc * c)
+}
+
+// Test Cases
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+    it("Testing for fixed tests", () => {
+        assert.strictEqual(grow([1, 2, 3]), 6);
+        assert.strictEqual(grow([4, 1, 1, 1, 4]), 16);
+        assert.strictEqual(grow([2, 2, 2, 2, 2, 2]), 64);
+    });
+})
